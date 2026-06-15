4452827b86d7f95afe8b827facb27020a55a120c
Your Name <you@example.com>
2026-06-15 19:01:34 +0530

    Version 1.0

---
+++ tests/codegenLensLogin.spec.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('Login', async ({ page }) => {
+  await page.goto('https://lens-staging.radiangen.com/');
+  await page.locator('body').click();
+  await page.goto('https://lens-staging.radiangen.com/#/login');
+  await page.getByRole('textbox', { name: 'E-mail' }).click();
+  await page.getByRole('textbox', { name: 'E-mail' }).fill('nelson1@radiangen.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('Nelson@123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('button', { name: 'No' }).click();
+});
